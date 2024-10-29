5d355ec847720d4060a4e1f899245cdce54f4fb9
q1p1 <meshari.algahbban@wakecap.com>
2024-10-29 11:03:04 +0300

    main

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 let imgBox = document.getElementById("imgBox");
 let qrimg = document.getElementById("qrimg");
 let qrText = document.getElementById("qrText");
+let codeType = "qr";
+
+function toggleCodeType() {
+  codeType = codeType === "qr" ? "barcode" : "qr";
+  document.querySelector("button[onclick='toggleCodeType()']").innerText =
+    codeType === "qr" ? "Switch to Barcode" : "Switch to QR Code";
+  qrmake();
+}
 
 function qrmake() {
-  qrimg.src =
-    "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" +
-    qrText.value;
+  const text = qrText.value;
+  if (codeType === "qr") {
+    //  QR Code
+    qrimg.src =
+      "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" +
+      encodeURIComponent(text);
+  } else {
+    // Barcode
+    qrimg.src =
+      "https://quickchart.io/barcode?type=upca&text=12345678900&includeText=true&rotate=R" +
+      encodeURIComponent(text) +
+      "&size=150";
+  }
 }
